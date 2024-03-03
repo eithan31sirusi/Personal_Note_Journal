5af528def4a4fd07506ac552670250b4f4657a65
eithan31sirusi <eithan26sirusi@gmail.com>
2024-03-03 18:38:57 +0200

    .

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-// consum the provider and pass it to the app ----
+// consum the provider and pass it to the app
 import { UserPageContextProvider } from "./setup/context/userPageContext";
 import { ModalContextProvider } from "./setup/context/modalContext";
 import { SelectDropDwonContextProvider } from "./setup/context/selectDropDwonContext";
